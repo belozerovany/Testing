8701aead0d4ac855e94f2b211a6be781a72d5b42
Nadia <belozerovany@gmail.com>
2017-11-14 21:39:53 +0200

    changed unit test for getRandomUsers

--- src/test3func-1task.js
+++ src/test3func-1task.js
@@ -1,36 +1,23 @@
 const testObj = {
-    equal(result) {
-      if (this.input === result) console.log('Success'); 
-      else console.error(`${this.input} not equals to ${result}` );
-    },
-    defined() {
-      if (this.input !== undefined) console.log('Success'); 
-      else console.error(`${this.input} is not defined` );
-    }
-  };
-  const test = (input) => {
-    testObj.input = input;
-    return testObj;
-  };
+  equal(result) {
+    if (this.input === result) console.log('Success'); 
+    else console.error(`${this.input} not equals to ${result}` );
+  },
+  defined() {
+    if (this.input !== undefined) console.log('Success'); 
+    else console.error(`${this.input} is not defined` );
+  } 
+};
+const test = (input) => {
+  testObj.input = input;
+  return testObj;
+};
   
- // test(sum(2, 3)).equal(5);
-  //test(sum()).defined();
-    
-
 //1
 const getDay = () => {
     return new Date().getDay();
 };
-
-const testDate = (result) => {
-    if ( result < 0 ) console.log(`${result} is not new Date().getDay().Today is ${new Date().getDay()} day`); 
-    else if (result <= 6 )console.log('success');
-    else console.log(`${result} is not new Date().getDay().Today is ${new Date().getDay()} day`);
-}
-testDate(getDay());
-//or
 test(getDay()).equal(new Date().getDay());
-
 
 //2
 const users = [{age: 15}, {age: 14}, {age: 28}, {age: 18}, {age: 45}, {age: 68}, {age: 38}, {age: 22}, {age: 14}];
@@ -42,6 +29,7 @@ test(getAdultUsers()).defined([]);
 
 //3
 const users = [{age: 15}, {age: 14}, {age: 28}, {age: 18}, {age: 45}, {age: 68}, {age: 38}, {age: 22}, {age: 14}];
+
 const getRandomUsers = (users) => {
     const numb = Math.random();
     console.log('numb',numb);
@@ -65,23 +53,9 @@ const getRandomUsers = (users) => {
   const originRandom = Math.random;
   Math.random = () => 0.8;
 
-  if(Math.random() > 0.5){
-    test(getRandomUsers(users).length).equal(users.slice(0, Math.round(users.length / 2)).length);
-  };
+  test(getRandomUsers(users).length).equal(5);
+  test(getRandomUsers(users).length).equal(Math.round(users.length / 2));
+  test(getRandomUsers(users)[0]).equal(users[0]);
+  test(getRandomUsers()).defined(false);
   Math.random = originRandom;  
 
-
-  const originRandomtwo = Math.random;
-  Math.random = () => 0.3;
-  if(Math.random() < 0.5){
-    test(getRandomUsers(users).length).equal(users.slice(Math.round(users.length / 2), users.length).length);
-  }
-  Math.random = originRandomtwo;  
-  
-  test(getRandomUsers()).defined(false);
-
- 
-//2
-const originRandom = Math.random;
-Math.random = () => 0.8;
-Math.random = originRandom;  
